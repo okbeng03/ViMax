95284b24213d0ee3a1983c7845d2d0e62e84f351
chb.wangcb <chb.wang@eking-tech.com>
2026-04-28 14:48:35 +0800

    feat: 百炼oss图片上传（短时效）

--- utils/image.py
+++ utils/image.py
@@ -2,9 +2,10 @@ import logging
 import requests
 import base64
 import mimetypes
+import os
 from tenacity import retry
+from pathlib import Path
 from io import BytesIO
-import cv2
 
 
 @retry
@@ -57,3 +58,58 @@ def save_base64_image(b64_string, save_path):
     with open(save_path, 'wb') as image_file:
         image_file.write(base64.b64decode(b64_string))
 
+# 百炼图片上传
+dashscope_api_key = os.getenv("DASHSCOPE_API_KEY")
+
+def get_upload_policy(model_name):
+    """获取文件上传凭证"""
+
+    url = "https://dashscope.aliyuncs.com/api/v1/uploads"
+    headers = {
+        "Authorization": f"Bearer {dashscope_api_key}",
+        "Content-Type": "application/json"
+    }
+    params = {
+        "action": "getPolicy",
+        "model": model_name
+    }
+    
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code != 200:
+        raise Exception(f"Failed to get upload policy: {response.text}")
+    
+    return response.json()['data']
+
+def upload_file_to_oss(policy_data, file_path):
+    """将文件上传到临时存储OSS"""
+
+    file_name = Path(file_path).name
+    key = f"{policy_data['upload_dir']}/{file_name}"
+    
+    with open(file_path, 'rb') as file:
+        files = {
+            'OSSAccessKeyId': (None, policy_data['oss_access_key_id']),
+            'Signature': (None, policy_data['signature']),
+            'policy': (None, policy_data['policy']),
+            'x-oss-object-acl': (None, policy_data['x_oss_object_acl']),
+            'x-oss-forbid-overwrite': (None, policy_data['x_oss_forbid_overwrite']),
+            'key': (None, key),
+            'success_action_status': (None, '200'),
+            'file': (file_name, file)
+        }
+        
+        response = requests.post(policy_data['upload_host'], files=files)
+        if response.status_code != 200:
+            raise Exception(f"Failed to upload file: {response.text}")
+    
+    return f"oss://{key}"
+
+def upload_file_and_get_url(model_name, file_path):
+    """上传文件并获取URL"""
+
+    # 1. 获取上传凭证，上传凭证接口有限流，超出限流将导致请求失败
+    policy_data = get_upload_policy(model_name) 
+    # 2. 上传文件到OSS
+    oss_url = upload_file_to_oss(policy_data, file_path)
+    
+    return oss_url
