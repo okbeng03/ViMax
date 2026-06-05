c48941f4ecd34697a12542fb8cd947233e94f638
chb.wangcb <chb.wang@eking-tech.com>
2026-06-05 14:09:26 +0800

    config: 默认音色

--- configs/config.py
+++ configs/config.py
@@ -1,1 +1,12 @@
-working_dir = "/Users/wangchangbin/ai/vimax_output6"
+working_dir = "/Users/wangchangbin/ai/cangjie"
+evaluate_agent_baseurl = "http://192.168.3.4:8000"
+evealute_project_id = "fdf55d15-388f-48e4-a75f-55608ca138a3"
+
+register_voices = {
+    "字博士": "字博士",
+    "小豆丁": "小豆丁",
+    "旁白": "旁白",
+    "default": "男生",
+    "Male": "男生",
+    "Female": "女生",
+}
